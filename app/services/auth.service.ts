e8db1832ac9433c9052b4d5b2f0650faf6d99fa3
jlooper <jen.looper@gmail.com>
2017-08-26 08:46:57 -0400

    auth service - to be kept clean

---
+++ app/services/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+
+//add your keys here
+export class AuthService {
+  static clarifaiKey = "";
+  static clarifaiUrl = "";
+  static googleKey = "";
+  static algoliaAppId = "";
+  static algoliaKey = "";  
+}
