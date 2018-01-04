1dda419660426deaae80772167608fa62bad5c9d
jlooper <jen.looper@gmail.com>
2018-01-04 15:08:47 -0500

    update

--- app/services/auth.service.ts
+++ app/services/auth.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from "@angular/core";
 
 //add your keys here
 export class AuthService {
-  static clarifaiKey = "bbb5c399091a40dba10d070d85179a47";
-  static clarifaiUrl = "https://api.clarifai.com/v2/models/bd367be194cf45149e75f01d59f77ba7/outputs";
-  static googleKey = "AIzaSyAJ_rSFTX-wjpA237VKPOVN7CpFn6DMJLU";
-  static algoliaAppId = "WZGM0BRSDB";
-  static algoliaKey = "8f69d6ada2d9c2040176b1941c675887";  
-  static deviceId = "270025001647343339383037";
+  static clarifaiKey = "";
+  static clarifaiUrl = "";
+  static googleKey = "";
+  static algoliaAppId = "";
+  static algoliaKey = "";  
+  static deviceId = "";
 }
